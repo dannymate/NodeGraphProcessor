8c3c6c9e6b59770e25360bf6ebb3e31b8d03c0e1
Daniel Burgess <developerdannymate@gmail.com>
2022-10-02 00:02:17 +0100

    FEAT VisualElementUtils are now factory pattern

--- Assets/com.alelievr.NodeGraphProcessor/Editor/Utils/VisualElementUtils.cs
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Utils/VisualElementUtils.cs
@@ -7,17 +7,19 @@ namespace GraphProcessor.Utils
         /// <summary>
         /// Make visible and affects layout.
         /// </summary>
-        public static void Show(this VisualElement ve)
+        public static VisualElement Show(this VisualElement ve)
         {
             ve.style.display = DisplayStyle.Flex;
+            return ve;
         }
 
         /// <summary>
         /// Make invisible and absent from layout.
         /// </summary>
-        public static void Hide(this VisualElement ve)
+        public static VisualElement Hide(this VisualElement ve)
         {
             ve.style.display = DisplayStyle.None;
+            return ve;
         }
 
         public static bool IsShowing(this VisualElement ve)
@@ -25,23 +27,41 @@ namespace GraphProcessor.Utils
             return ve.style.display == DisplayStyle.Flex;
         }
 
-        public static void SetPosition(this VisualElement ve, float top, float bottom, float left, float right)
+        public static VisualElement SetPosition(this VisualElement ve, float top, float bottom, float left, float right)
         {
             ve.style.top = top;
             ve.style.bottom = bottom;
             ve.style.left = left;
             ve.style.right = right;
+            return ve;
         }
 
-        public static void SetSize(this VisualElement ve, float width, float height)
+        public static VisualElement SetSize(this VisualElement ve, float width, float height)
         {
             ve.style.width = width;
             ve.style.height = height;
+            return ve;
         }
 
-        public static void SetOpacity(this VisualElement ve, float opacity)
+        public static VisualElement SetOpacity(this VisualElement ve, float opacity)
         {
             ve.style.opacity = opacity;
+            return ve;
+        }
+
+        public static VisualElement SetMargin(this VisualElement ve, float top, float bottom, float left, float right)
+        {
+            ve.style.marginTop = top;
+            ve.style.marginBottom = bottom;
+            ve.style.marginLeft = left;
+            ve.style.marginRight = right;
+            return ve;
+        }
+
+        public static VisualElement SetAlignment(this VisualElement ve, Align alignment)
+        {
+            ve.style.alignSelf = alignment;
+            return ve;
         }
     }
 }
